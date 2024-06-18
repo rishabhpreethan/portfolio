4e11efcd6d93c2e92a0867ea8ea753d3bfdaa8ba
rishabhpreethan <rishabh.preethan@gmail.com>
2024-06-18 23:08:42 +0530

    Added Custom cursor

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,14 @@ import LandingPage from './components/LandingPage';
 import Name from './components/Name';
 import Test from './components/Test'
 import FirstPage from './components/FirstPage_temp';
+import CustomCursor from './components/CustomCursor';
+import MenuBar from './components/MenuBar';
 
 const App = () => {
   return (
     <div>
+      <CustomCursor />
+      {/* <MenuBar /> */}
       <FirstPage />
       {/* <LandingPage /> */}
       {/* <Name /> */}
